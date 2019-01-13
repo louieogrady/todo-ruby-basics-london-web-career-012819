e1e523b07e69972b3b7547d8d79d2f1851dd74a2
Louie O'Grady <l.ogrady@gmail.com>
2019-01-13 17:12:30 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong!")
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts "Hi there, #{name}!"
 end
 
 def return_a_value
